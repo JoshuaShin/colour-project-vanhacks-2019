7f076039a5e376ba890a9e3f61fc48be40f41ef0
gordontsui <tsui.gordon.9@gmail.com>
2019-09-14 12:36:57 -0700

    Initial commit of Grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Grid from './components/Grid'
 import './App.css';
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
 
 function App() {
   return (
     <div className="App">
-     hello world
+        <Grid />
     </div>
   );
 }

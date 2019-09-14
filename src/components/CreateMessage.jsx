e35055f79f07a02e90400d7e112b5ef0ad25f708
JoshuaShin <joshua_shin@hotmail.com>
2019-09-14 15:15:01 -0700

    Add color selection to modal window

--- src/components/CreateMessage.jsx
+++ src/components/CreateMessage.jsx
@@ -4,15 +4,16 @@ import { Form, TextArea, Button, Segment } from 'semantic-ui-react'
 
 export default class CreateMessage extends Component {
 
+    yourStory = "";
+
     constructor (props) {
         super(props)
         this.state = {
             inputValue: '',
             backgroundColor: '',
-            yourStory: '',
-            yourColor: ''
         }
     }
+
     onChange(inputEntry) {
         if (inputEntry) {
             this.setState({inputValue: inputEntry, backgroundColor: '#FF0000'}) // here I want to change the color to red
@@ -28,14 +29,22 @@ export default class CreateMessage extends Component {
             <Popup trigger={<button className="button"> Open Modal </button>} modal>
                 {close => (
                     <div className="modal" style={{backgroundColor: backgroundColor}}>
-                        <a className="close" onClick={close}>
-                            &times;
-                        </a>
+
+
+                        /* ===== Directions ===== */
+
+                        {/*<a className="close" onClick={close}>*/}
+                        {/*    &times;*/}
+                        {/*</a>*/}
                         {/*<div className="header"> Title </div>*/}
                         <div className="content">
                             {" "}
-                            Share your story with us - a message you would like to share with everyone, your story of hope, or any word of encouragement!
+                            Share your story with us!
                         </div>
+
+
+                        /* ===== Input fields ===== */
+
                         <Form>
                             <label>*Your Story</label>
                             <TextArea placeholder='Your story' style={{ minHeight: 100 }} />
@@ -46,72 +55,74 @@ export default class CreateMessage extends Component {
                         </Form>
 
 
+                        /* ===== Color selection ===== */
+
                         <div>
                             <Segment>
                                 <Button
                                     inverted color='red'
                                     onClick={() => {
-                                        this.setState({backgroundColor: '#FF0000'}) // here I want to change the color to red
+                                        this.setState({backgroundColor: '#ff695e'}) // here I want to change the color to red
                                     }}
                                 >
                                     Anger
                                 </Button>
-                                {/*<Button inverted color='orange'>*/}
-                                {/*    Orange*/}
-                                {/*</Button>*/}
-                                <Button inverted color='yellow'>
+                                <Button
+                                    inverted color='yellow'
+                                    onClick={() => {
+                                        this.setState({backgroundColor: '#ffe21f'}) // here I want to change the color to red
+                                    }}
+                                >
                                     Joy
                                 </Button>
-                                {/*<Button inverted color='olive'>*/}
-                                {/*    Olive*/}
-                                {/*</Button>*/}
-                                <Button inverted color='green'>
+                                <Button
+                                    inverted color='green'
+                                    onClick={() => {
+                                        this.setState({backgroundColor: '#2ecc40'}) // here I want to change the color to red
+                                    }}
+                                >
                                     Peace
                                 </Button>
-                                {/*<Button inverted color='teal'>*/}
-                                {/*    Teal*/}
-                                {/*</Button>*/}
-                                <Button inverted color='blue'>
+                                <Button
+                                    inverted color='blue'
+                                    onClick={() => {
+                                        this.setState({backgroundColor: '#54c8ff'}) // here I want to change the color to red
+                                    }}
+                                >
                                     Sad
                                 </Button>
-                                <Button inverted color='violet'>
+                                <Button
+                                    inverted color='violet'
+                                    onClick={() => {
+                                        this.setState({backgroundColor: '#a291fb'}) // here I want to change the color to red
+                                    }}
+                                >
                                     Anxious
                                 </Button>
-                                {/*<Button inverted color='purple'>*/}
-                                {/*    Purple*/}
-                                {/*</Button>*/}
-                                {/*<Button inverted color='pink'>*/}
-                                {/*    Pink*/}
-                                {/*</Button>*/}
-                                {/*<Button inverted color='brown'>*/}
-                                {/*    Brown*/}
-                                {/*</Button>*/}
-                                {/*<Button inverted color='grey'>*/}
-                                {/*    Grey*/}
-                                {/*</Button>*/}
                             </Segment>
                         </div>
 
 
+                        /* ===== Submit button ===== */
+
                         <div className="actions">
-                            <Popup
-                                trigger={<button className="button"> color </button>}
-                                position="top center"
-                                closeOnDocumentClick
-                            >
-                                <span>
-                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-                                  magni omnis delectus nemo, maxime molestiae dolorem numquam
-                                  mollitia, voluptate ea, accusamus excepturi deleniti ratione
-                                  sapiente! Laudantium, aperiam doloribus. Odit, aut.
-                                </span>
-                            </Popup>
+                            {/*<Popup*/}
+                            {/*    trigger={<button className="button"> color </button>}*/}
+                            {/*    position="top center"*/}
+                            {/*    closeOnDocumentClick*/}
+                            {/*>*/}
+                            {/*    <span>*/}
+                            {/*      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae*/}
+                            {/*      magni omnis delectus nemo, maxime molestiae dolorem numquam*/}
+                            {/*      mollitia, voluptate ea, accusamus excepturi deleniti ratione*/}
+                            {/*      sapiente! Laudantium, aperiam doloribus. Odit, aut.*/}
+                            {/*    </span>*/}
+                            {/*</Popup>*/}
                             <button
                                 className="button"
                                 onClick={() => {
-                                    this.onChange(true);
-                                    console.log("modal closed ");
-                                    //close();
+                                    this.setState({backgroundColor: '#ffffff'}) // here I want to change the color to red
+                                    close();
                                 }}
                             >
                                 submit
